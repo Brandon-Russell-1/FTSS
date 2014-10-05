acab45c911fea026498d66b64319f3e773d0457d
Jeff McCoy <code@jeffandshauna.net>
2014-10-05 00:54:49 -0400

    ****SELECTIZE customization merger*****
    merging the old custom pieces of Selectize into the updated version.

--- bower_components/selectize/dist/js/selectize.js
+++ bower_components/selectize/dist/js/selectize.js
@@ -155,6 +155,7 @@
 	 * @returns {string}
 	 */
 	var escape_html = function(str) {
+		return str;
 		return (str + '')
 			.replace(/&/g, '&amp;')
 			.replace(/</g, '&lt;')
@@ -1460,22 +1461,24 @@
 			}
 	
 			for (i = 0; i < n; i++) {
-				option      = self.options[results.items[i].id];
-				option_html = self.render('option', option);
-				optgroup    = option[self.settings.optgroupField] || '';
-				optgroups   = $.isArray(optgroup) ? optgroup : [optgroup];
+				try {
+					option = self.options[results.items[i].id];
+					option_html = self.render('option', option);
+					optgroup = option[self.settings.optgroupField] || '';
+					optgroups = $.isArray(optgroup) ? optgroup : [optgroup];
 
-				for (j = 0, k = optgroups && optgroups.length; j < k; j++) {
-					optgroup = optgroups[j];
-					if (!self.optgroups.hasOwnProperty(optgroup)) {
-						optgroup = '';
+					for (j = 0, k = optgroups && optgroups.length; j < k; j++) {
+						optgroup = optgroups[j];
+						if (!self.optgroups.hasOwnProperty(optgroup)) {
+							optgroup = '';
+						}
+						if (!groups.hasOwnProperty(optgroup)) {
+							groups[optgroup] = [];
+							groups_order.push(optgroup);
+						}
+						groups[optgroup].push(option_html);
 					}
-					if (!groups.hasOwnProperty(optgroup)) {
-						groups[optgroup] = [];
-						groups_order.push(optgroup);
-					}
-					groups[optgroup].push(option_html);
-				}
+				}catch (e) {}
 			}
 	
 			// render optgroup headers & join groups
@@ -2432,7 +2435,7 @@
 		plugins: [],
 		delimiter: ',',
 		persist: true,
-		diacritics: true,
+		diacritics: false,
 		create: false,
 		createOnBlur: false,
 		createFilter: null,
@@ -2442,7 +2445,7 @@
 		maxItems: null,
 		hideSelected: null,
 		addPrecedence: false,
-		selectOnTab: false,
+		selectOnTab: true,
 		preload: false,
 		allowEmptyOption: false,
 	
@@ -2451,14 +2454,14 @@
 	
 		dataAttr: 'data-data',
 		optgroupField: 'optgroup',
-		valueField: 'value',
-		labelField: 'text',
+		valueField: 'Id',
+		labelField: 'label',
 		optgroupLabelField: 'label',
 		optgroupValueField: 'value',
 		optgroupOrder: null,
 	
-		sortField: '$order',
-		searchField: ['text'],
+		sortField: 'label',
+		searchField: ['search'],
 		searchConjunction: 'and',
 	
 		mode: null,
@@ -2735,98 +2738,85 @@
 		})();
 	
 	});
-	
-	Selectize.define('optgroup_columns', function(options) {
+
+	/**
+	 * Plugin: "optgroup_columns" (selectize.js)
+	 * Copyright (c) 2013 Simon Hewitt & contributors
+	 *
+	 * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+	 * file except in compliance with the License. You may obtain a copy of the License at:
+	 * http://www.apache.org/licenses/LICENSE-2.0
+	 *
+	 * Unless required by applicable law or agreed to in writing, software distributed under
+	 * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+	 * ANY KIND, either express or implied. See the License for the specific language
+	 * governing permissions and limitations under the License.
+	 *
+	 * @author Simon Hewitt <si@sjhewitt.co.uk>
+	 */
+	Selectize.define('optgroup_columns', function() {
 		var self = this;
-	
-		options = $.extend({
-			equalizeWidth  : true,
-			equalizeHeight : true
-		}, options);
-	
+
 		this.getAdjacentOption = function($option, direction) {
 			var $options = $option.closest('[data-group]').find('[data-selectable]');
-			var index    = $options.index($option) + direction;
-	
+			var index = $options.index($option) + direction;
+
 			return index >= 0 && index < $options.length ? $options.eq(index) : $();
 		};
-	
+
 		this.onKeyDown = (function() {
 			var original = self.onKeyDown;
 			return function(e) {
 				var index, $option, $options, $optgroup;
-	
+
 				if (this.isOpen && (e.keyCode === KEY_LEFT || e.keyCode === KEY_RIGHT)) {
 					self.ignoreHover = true;
 					$optgroup = this.$activeOption.closest('[data-group]');
 					index = $optgroup.find('[data-selectable]').index(this.$activeOption);
-	
-					if(e.keyCode === KEY_LEFT) {
+
+					if (e.keyCode === KEY_LEFT) {
 						$optgroup = $optgroup.prev('[data-group]');
 					} else {
 						$optgroup = $optgroup.next('[data-group]');
 					}
-	
+
 					$options = $optgroup.find('[data-selectable]');
-					$option  = $options.eq(Math.min($options.length - 1, index));
+					$option = $options.eq(Math.min($options.length - 1, index));
 					if ($option.length) {
 						this.setActiveOption($option);
 					}
 					return;
 				}
-	
+
 				return original.apply(this, arguments);
 			};
 		})();
-	
-		var getScrollbarWidth = function() {
-			var div;
-			var width = getScrollbarWidth.width;
-			var doc = document;
-	
-			if (typeof width === 'undefined') {
-				div = doc.createElement('div');
-				div.innerHTML = '<div style="width:50px;height:50px;position:absolute;left:-50px;top:-50px;overflow:auto;"><div style="width:1px;height:100px;"></div></div>';
-				div = div.firstChild;
-				doc.body.appendChild(div);
-				width = getScrollbarWidth.width = div.offsetWidth - div.clientWidth;
-				doc.body.removeChild(div);
-			}
-			return width;
-		};
-	
+
 		var equalizeSizes = function() {
-			var i, n, height_max, width, width_last, width_parent, $optgroups;
-	
+			var n, $optgroups;
+
 			$optgroups = $('[data-group]', self.$dropdown_content);
 			n = $optgroups.length;
-			if (!n || !self.$dropdown_content.width()) return;
-	
-			if (options.equalizeHeight) {
-				height_max = 0;
-				for (i = 0; i < n; i++) {
-					height_max = Math.max(height_max, $optgroups.eq(i).height());
-				}
-				$optgroups.css({height: height_max});
+			if (!n || !self.$dropdown_content.width()) {
+				return;
 			}
-	
-			if (options.equalizeWidth) {
-				width_parent = self.$dropdown_content.innerWidth() - getScrollbarWidth();
-				width = Math.round(width_parent / n);
-				$optgroups.css({width: width});
-				if (n > 1) {
-					width_last = width_parent - width * (n - 1);
-					$optgroups.eq(n - 1).css({width: width_last});
-				}
-			}
+
+			// Use CSS to handle width stuff for us but
+			_($optgroups).each(function(opt) {
+
+				opt.className = [
+					'optgroup',
+					'count-' + $optgroups.length,
+					opt.getAttribute('data-group').toLowerCase().replace(/\s/g, '')
+				].join(' ');
+
+			});
+
 		};
-	
-		if (options.equalizeHeight || options.equalizeWidth) {
-			hook.after(this, 'positionDropdown', equalizeSizes);
-			hook.after(this, 'refreshOptions', equalizeSizes);
-		}
-	
-	
+
+		hook.after(this, 'positionDropdown', equalizeSizes);
+		hook.after(this, 'refreshOptions', equalizeSizes);
+
 	});
 	
 	Selectize.define('remove_button', function(options) {
