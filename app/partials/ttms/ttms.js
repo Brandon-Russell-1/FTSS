c85e376a04febf0b9389649e1810bd4217ebefb9
Jeff McCoy <code@jeffandshauna.net>
2014-10-19 23:26:09 -0400

    Refactored row update code to inlineEdit Function
    This will probably be generalized some more later on and pushed out to
    the utils or even FTSS.controllers objects as it has some really handy
    features.

--- app/partials/ttms/ttms.js
+++ app/partials/ttms/ttms.js
@@ -10,26 +10,68 @@ FTSS.ng.controller(
 
 			var self = FTSS.controller($scope, {
 
-				'sort' : 'age',
-				'group': 'location',
+				    'sort' : 'age',
+				    'group': 'location',
 
-				'grouping': {
-					'location': 'Location'
-				},
+				    'grouping': {
+					    'location': 'Location'
+				    },
 
-				'sorting': {
+				    'sorting': {
 
-					'Start': 'Start Date',
-					'age'  : 'Age'
+					    'Start': 'Start Date',
+					    'age'  : 'Age'
 
-				},
+				    },
 
-				'model' : 'ttms',
+				    'model' : 'ttms',
 
-				// We only want classes with requests
-				'filter': "TTMS eq null and Archived eq false"
+				    // We only want classes with requests
+				    'filter': "TTMS eq null and Archived eq false"
 
-			});
+			    });
+
+			    $scope.inlineUpdate = function (row, field, setArchive) {
+
+				    if (_.isEmpty(row[field])) {
+					    return;
+				    }
+
+				    var send = {
+					    'cache'     : true,
+					    '__metadata': row.__metadata
+				    };
+
+				    send[field] = row[field];
+
+				    // Call sharePoint.update() with our data and handle the success/failure response
+				    SharePoint
+
+					    .update(send)
+
+					    .then(function (resp) {
+
+						          // HTTP 204 is the status given for a successful update, there will be no body
+						          if (resp.status === 204) {
+
+							          utils.alert.update();
+
+							          if (setArchive) {
+								          row.Archived = true;
+							          }
+
+							          self.process();
+
+						          } else {
+
+							          utils.alert.error('Please try again later.');
+
+						          }
+
+					          });
+
+
+			    };
 
 			self
 
@@ -39,51 +81,9 @@ FTSS.ng.controller(
 
 					      $scope.edit = angular.noop;
 
-					      $scope.addTTMS = function (row) {
-
-						      if (_.isEmpty(row.TTMS)) {
-							      return;
-						      }
-
-						      // Call sharePoint.update() with our data and handle the success/failure response
-						      SharePoint
-
-							      .update({
-								              'cache'     : true,
-								              '__metadata': row.__metadata,
-								              'TTMS'      : row.TTMS
-							              })
-
-							      .then(function (resp) {
-
-								            // HTTP 204 is the status given for a successful update, there will be no body
-								            if (resp.status === 204) {
-
-									            utils.alert.update();
-
-									            row.Archived = true;
-
-									            self.process();
-
-								            } else {
-
-									            utils.alert.error('Please try again later.');
-
-								            }
-
-							            });
-
-					      };
-
 					      self.initialize(data).then(function (row) {
 
 						      utils.cacheFiller(row);
-
-						      if (row.ClassNotes) {
-
-							      row.ClassNotes = 'FTD: ' + row.ClassNotes;
-
-						      }
 
 						      row.age = moment(row.Created).fromNow();
 
