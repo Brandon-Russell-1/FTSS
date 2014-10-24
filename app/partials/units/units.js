c791a852dc3d228bd4bafb2380933902e9a0ddb0
Jeff McCoy <code@jeffandshauna.net>
2014-10-24 02:51:36 -0400

    Removed hover info from FTD view
    This was never really that useful and caused a lot of headaches.

--- app/partials/units/units.js
+++ app/partials/units/units.js
@@ -51,64 +51,11 @@ FTSS.ng.controller(
 
 			});
 
-			self
+			self.bind().then(function (data) {
 
-				.bind()
+				self.initialize(data).then();
 
-				.then(function (data) {
-
-					      self
-
-						      .initialize(data)
-
-						      .then(function (unit) {
-
-							            // Flatten Instructors into string
-
-							            unit.Instructors = _.where(caches.Instructors, {'UnitId': unit.Id});
-
-							            unit.InstructorsList = _.pluck(unit.Instructors, 'label').sort().join('<br>');
-
-							            unit.Squadron = unit.Det < 300 ? '372 TRS' : '373 TRS';
-
-							            if (unit.Courses_JSON) {
-
-								            unit.Courses = [];
-
-								            _(unit.Courses_JSON).each(function (course) {
-
-									            unit.Courses.push(caches.MasterCourseList[course]);
-
-								            });
-
-								            // Flattened string (yes this is HTML in a controller :-( for Hover
-								            unit.CoursesHover = _.map(unit.Courses, function (c) {
-
-									            if (unit.Courses.length > 10) {
-										            return '<div class="col-lg-4" hover="' +
-										                   c.Title +
-										                   '" left><b>' +
-										                   c.PDS +
-										                   '</b>: ' +
-										                   c.Number +
-										                   '</div>';
-									            } else {
-										            return '<dt class="tiny">' +
-										                   c.PDS +
-										                   '</dt><dd>' +
-										                   c.Number +
-										                   '<br><small class="truncate">' +
-										                   c.Title +
-										                   '</small></dd>';
-									            }
-
-								            }).join('');
-
-							            }
-
-						            });
-
-				      });
+			});
 
 		}
 	]);
