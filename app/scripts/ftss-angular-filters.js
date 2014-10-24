57ad171de2bfef36cf97f943f4c09711499be81e
Jeff McCoy <code@jeffandshauna.net>
2014-10-24 01:54:53 -0400

    Removed first name filter

--- app/scripts/ftss-angular-filters.js
+++ app/scripts/ftss-angular-filters.js
@@ -20,22 +20,11 @@
 		};
 	});
 
-	/**
-	 * Quick filter to return the first name of a person (AF GAL format)
-	 */
-	FTSS.ng.filter('firstName', function () {
-		return function (input) {
-			var match = input && input.match(/[a-z]+,\s([a-z]+)/i);
-			return match && match[1] || input || 'John or Jane Doe';
-		};
-	});
-
 	FTSS.ng.filter('courseNumber', function () {
 		return function (input) {
 			return input || 'Some Random Course?';
 		};
 	});
-
 
 	FTSS.ng.filter('search', function () {
 
