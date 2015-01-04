55d5980ad2292c7b8b612e5ce917a1a1254be78d
Jeff McCoy <code@jeffandshauna.net>
2015-01-04 13:50:39 -0500

    Commented scheduled-row-processing

--- app/partials/scheduled/scheduled-row-processing.js
+++ app/partials/scheduled/scheduled-row-processing.js
@@ -127,36 +127,47 @@
 
 	utils.processScheduledRow = function (row) {
 
+		// For fake courses (unavalability events), we only need to load a few pieces of data
 		if (!row.CourseId || row.CourseId < 1) {
 
+			// Make sure the CourseId is -1, it will be null coming from the server
 			row.CourseId = -1;
 
+			// Add our class for this event
 			row.className = 'ignore';
 
+			// Try to load the instructor's data
 			row.Instructor = caches.Instructors[row.InstructorId] || {};
 
+			// Only search by instructor name + unavailable
 			row.search = row.Instructor.InstructorName + ' unavailable';
 
+			// Calculate the start time
 			row.startMoment = moment(row.Start);
 
+			// Calculate the end time
 			row.endMoment = row.startMoment.clone().add(row.Days - 1, 'days');
 
 		} else {
 
+			// Run through the cacheFiller
 			utils.cacheFiller(row);
 
+			// Determine classes (color codes) based on openSeats
 			row.className = row.openSeats > 0 ? 'success' :
 
 			                row.openSeats < 0 ? 'danger' :
 
 			                'warning';
 
+			// Map the status to our colors codes
 			row.availability = {
 				'success': 'Open Seats',
 				'warning': 'No Open Seats',
 				'danger' : 'Seat Limit Exceeded'
 			}[row.className];
 
+			// Cleanup the TTMS presentation
 			row.TTMSText = row.TTMS ? ' - ' + row.TTMS : '';
 
 			row.title = [
@@ -165,6 +176,7 @@
 				row.TTMS || 'Pending Class #'
 			].join(' - ');
 
+			// Setup our search fields for this view
 			row.search = [
 				row.ClassNotes,
 				row.Course.text,
@@ -174,8 +186,10 @@
 				row.startMoment.format('MMMM')
 			].join(' ');
 
+			// Auto archive (in the view) older classes
 			row.Archived = row.Archived || row.endMoment.isBefore(lastWeek);
 
+			// The URL for our mailTo link
 			row.mailFTD = row.FTD.Email +
 			              '?subject=FTSS Class Inquiry for ' +
 			              row.Course.PDS +
@@ -186,6 +200,9 @@
 			row.map = 'https://maps.googleapis.com/maps/api/staticmap?' +
 			          'sensor=false&size=400x300&zoom=5&markers=color:red|' +
 			          row.FTD.Location.replace(/\s/g, '');
+
+			// Hide the J4 notes if they have the leading #
+			row.J4Notes =  (row.J4Notes && row.J4Notes[0] === '#') ? '' : row.J4Notes;
 
 		}
 
