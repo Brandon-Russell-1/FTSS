72c8dcd9b0aa445919bbd1189117e81e45837b11
Jeff McCoy <code@jeffandshauna.net>
2015-01-06 18:22:08 -0800

    Added J4 Email

--- app/scripts/ftss-init.js
+++ app/scripts/ftss-init.js
@@ -114,8 +114,9 @@
 	// Init our utils objects as it is used in various other JS files
 	FTSS.utils = {};
 
-	// This probably doesn't belong here?
+	// This probably doesn't belong here? :-D
 	FTSS.supportEmail = '372trs.trg.ftss@us.af.mil';
+	FTSS.J4Email = '982TRG.J4scheduling@us.af.mil';
 
 	// Helper variable for handling production vs development mode photos
 	FTSS.photoURL = PRODUCTION ? base + 'live/Bios/' : base + 'development/Bios/';
