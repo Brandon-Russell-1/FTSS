e2cfad09a21cb7563aa3b71db3c97e0c71a73bdc
Jeff McCoy <code@jeffandshauna.net>
2015-01-20 00:19:19 -0500

    Simplifying the production-ftd model

--- app/partials/production-ftd/production-ftd.js
+++ app/partials/production-ftd/production-ftd.js
@@ -15,28 +15,18 @@ FTSS.ng.controller(
 
 				'sort' : 'InstructorName',
 				'model': 'instructors',
-				'modal': 'instructor-stats',
-
-				'noFilter': true
+				'modal': 'instructor-stats'
 
 			});
 
-			// We use $scope.edit's modal function to show instructor stats
-			$scope.stats = function () {
-
-				// We have to pass our context to $scope.edit for the instructor stats
-				$scope.edit.apply(this);
-
-			};
-
-			self.bind().then(function (data) {
+			self.bind('ftd').then(function (data) {
 
 				var UnitId = $scope.ftd.Id,
 
 				    read = _.clone(FTSS.models.scheduled);
 
 				// Only include unarchived instructors for this unit
-				data = angular.copy(_.filter(data, {'UnitId': UnitId, 'Archived': false}));
+				data = angular.copy(_.filter(data, {'Archived': false}));
 
 				// Only include this unit
 				read.params.$filter = '(UnitId eq ' + UnitId + ')';
