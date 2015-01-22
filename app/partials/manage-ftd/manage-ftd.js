461c644e36ad5bb862a8e81f43f2760e9903c57b
Jeff McCoy <code@jeffandshauna.net>
2015-01-21 22:07:46 -0500

    Removed all stats/reporting from manage-ftd
    This will make this page much less frequented (which is a good thing)
    and ensures all production/stats info is found in the same place
    (production-ftd).

--- app/partials/manage-ftd/manage-ftd.js
+++ app/partials/manage-ftd/manage-ftd.js
@@ -22,27 +22,6 @@ FTSS.ng.controller(
 
 			});
 
-			// This allows us to copy & paste lists of PDS codes for an FTD
-			FTSS.pasteAction = function (text) {
-
-				var match = text.toUpperCase().match(/(\w+)/g),
-
-				    courses = _(angular.copy(caches.MasterCourseList)),
-
-				    collection = [];
-
-				_(match).each(function (val) {
-
-					var valid = courses.find({'PDS': val});
-
-					valid && collection.push(valid.Id);
-
-				});
-
-				collection.length && FTSS.selectizeInstances.Courses_JSON.setValue(collection);
-
-			};
-
 			// This handles our drag & drop photo operations
 			$scope.onFileSelect = function ($files) {
 
@@ -142,114 +121,17 @@ FTSS.ng.controller(
 
 			self.bind().then(function (data) {
 
-				var UnitId = $scope.ftd.Id,
-
-				    read = _.clone(FTSS.models.scheduled),
-
-				    nextWeek = moment().add(7, 'days'),
-
-				    yearStart = moment().add(-1, 'years');
+				var UnitId = $scope.ftd.Id;
 
 				$scope.data = angular.copy(caches.Units[UnitId]);
 
 				// Only include instructors for this unit
 				data = angular.copy(_.filter(data, {'UnitId': UnitId}));
 
-				// Only include this unit
-				read.params.$filter = '(UnitId eq ' + UnitId + ')';
+				// Complete the controller initialization
+				self.initialize(data).then(function (row) {
 
-				// Request the scheduled data for this unit
-				SharePoint.read(read).then(function (results) {
-
-					var stats = _(results)
-
-						// Ignore our instructor unavailability
-						.reject({'CourseId': null})
-
-						// Load the cache data for every row (this one is a little expensive)
-						.each(utils.cacheFiller)
-
-						// Group the data by InstructorID
-						.groupBy('InstructorId')
-
-						// Return the chained value output from lodash
-						.value();
-
-					// Complete the controller initialization
-					self.initialize(data).then(function (row) {
-
-						var stat = stats[row.Id],
-
-						    chart = [];
-
-						row.search = row.InstructorName;
-
-						// for aggregate instructor stats
-						row.stats = {
-							'Instructor Hours': 0,
-							'Classes Taught'  : 0,
-							'Total Students'  : 0
-						};
-
-						row.annualHours = 0;
-
-						// add the data back to the scope
-						row.history = stat;
-
-						while (chart.length < 12) {
-							chart.push(0);
-						}
-
-						_(stat).each(function (course) {
-
-							// Tally all courses taught
-							row.stats['Classes Taught']++;
-
-							// Tally instructor hours, looking for a manual hours override first
-							row.stats['Instructor Hours'] += course.Hours || course.Course.Hours;
-
-							// Tally all students taught
-							row.stats['Total Students'] += course.allocatedSeats;
-
-							// If course was taught in the last year, count hours for annualHours
-							if (course.startMoment > yearStart && course.startMoment < nextWeek) {
-
-								chart[course.startMoment.month()] += course.Course.Hours;
-
-								row.annualHours += course.Course.Hours;
-
-							}
-						});
-
-						(function () {
-
-							var max = _.max(chart),
-
-							    months = 'Jan.Feb.Mar.Apr.May.Jun.Jul.Aug.Sep.Oct.Nov.Dec'.split('.');
-
-							row.chart = '';
-
-							_(chart).each(function (item, index) {
-
-								var pct = item ? Math.round((item / max) * 100) : 0;
-
-								row.chart += '<b><em style="height:' + pct + '%">&nbsp;</em>' +
-
-								             '<i>' + months[index] + '</i></b>';
-
-
-							});
-
-						}());
-
-						// A rough estimate of instructor time utilization
-						row.annualEffectiveness = Math.floor(row.annualHours / 19.2);
-
-						row.InstructorEmail = row.InstructorEmail ?
-						                      row.InstructorEmail.replace('mailto:', '') :
-						                      '';
-
-					});
+					row.search = row.InstructorName;
 
 				});
 
