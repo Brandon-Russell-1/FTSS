3361ccb60de4a85709b028c31e6cebb5657e77ea
Jeff McCoy <code@jeffandshauna.net>
2015-01-25 03:34:26 -0500

    Clean/refactor scheduled-search (removed old cached data code)

--- app/partials/scheduled/search/scheduled-search.js
+++ app/partials/scheduled/search/scheduled-search.js
@@ -14,11 +14,92 @@ FTSS.ng.controller(
 				    'group': 'Course.Number',
 				    'model': 'scheduledSearch'
 
-			    }),
+			    });
 
-			    today = moment();
+			$scope.autoApprove = $scope.hasRole
+			([
+				 'ftd',
+				 'scheduling'
+			 ]);
 
-			$scope.request = utils.requestSeats($scope, $modal, self);
+			$scope.canRequest = $scope.hasRole
+			([
+				 'mtf',
+				 'ftd',
+				 'scheduling'
+			 ]);
+
+			$scope.request = function() {
+
+				var row = this;
+
+				if ($scope.canRequest && row.openSeats > 0 || $scope.autoApprove) {
+
+					var scope = $scope.$new();
+
+					scope.data = row;
+
+					scope.data.Students = [];
+
+					scope.close = $modal(
+						{
+
+							'scope'          : scope,
+							'backdrop'       : 'static',
+							'contentTemplate': '/partials/modal-request-seats.html'
+
+						}).destroy;
+
+					scope.submit = function () {
+
+						if (!$scope.autoApprove) {
+
+							// Send our email notification to the FTD
+							utils.sendEmail(
+								{
+									'to'     : row.FTD.Email,
+									'subject': 'New Seat Request for ' + row.Course.PDS,
+									'body'   : caches.Hosts[scope.data.HostId].Unit +
+									         ' has requested ' +
+									         scope.data.Students.length +
+									         ' seats for the ' +
+									         row.dateRange +
+									         ' class:' +
+									         '\n\n' + scope.data.Students.join('\n') +
+									         '\n\n' + scope.data.Notes
+								});
+
+						}
+
+						row.Requests_JSON = row.Requests_JSON || [];
+
+						row.Requests_JSON.push(
+							[
+								// Status
+								scope.autoApprove ? 2 : 1,
+
+								// Students Array
+								scope.data.Students,
+
+								// Notes
+								scope.data.Notes,
+
+								// Host ID
+								scope.data.HostId
+							]);
+
+						self._update(scope, {
+
+							'cache'        : true,
+							'__metadata'   : row.__metadata,
+							'Requests_JSON': row.Requests_JSON
+
+						}, scope.close);
+
+					};
+				}
+
+			};
 
 			self
 
@@ -26,35 +107,12 @@ FTSS.ng.controller(
 
 				.then(function (data) {
 
-					      $scope.autoApprove = $scope.hasRole
-					      (['ftd',
-					        'scheduling'
-					       ]);
+					      self.initialize(data).then(function (row) {
 
-					      $scope.canRequest = $scope.hasRole(
-						      ['mtf',
-						       'ftd',
-						       'scheduling'
-						      ]);
+						      utils.processScheduledRow(row);
 
-					      self.initialize(data).then(function (row, key, collection) {
-
-						      // Delete if this class is cancelled or just unavailability
-						      if (!row.CourseId || row.Archived) {
-
-							      delete collection[row.Id];
-
-						      } else {
-
-							      utils.processScheduledRow(row);
-
-							      row.Archived = row.openSeats < 1;
-
-							      // Also delete if this is an old class
-							      if (row.startMoment.diff(today, 'days') < 0) {
-								      delete collection[row.Id];
-							      }
-						      }
+						      // Hide full classes by default
+						      row.Archived = (row.openSeats < 1);
 
 					      });
 
