1f2eef146e89d1f821ee338f09b71aab034b1e56
Jeff McCoy <code@jeffandshauna.net>
2015-02-11 00:02:34 -0500

    chore: updated production build process for gzip/aws/failover

--- scripts/production.sh
+++ scripts/production.sh
@@ -2,3 +2,25 @@
 
 rm -rf _public
 node_modules/.bin/brunch build -P
+
+cd _public
+
+mkdir failover
+mkdir aws
+
+cd js
+
+echo Building live-libs
+cp vendor.js ../failover/live-libs.js
+cat vendor.js | gzip -9cv > ../aws/live-libs.js
+
+echo Building live-apps
+cat partials.js app.js > ../failover/live-apps.js
+gzip -9cv ../failover/live-apps.js > ../aws/live-app.js
+
+cd ../css
+
+echo Builidng live-css
+cp app.css ../failover/live.css
+gzip -9cv app.css > ../aws/live.css
+
