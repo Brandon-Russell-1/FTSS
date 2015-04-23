06c777210dd253dd0437c11c4b2f3cdf6267d024
Jeff McCoy <code@jeffandshauna.net>
2015-04-23 19:24:20 -0400

    refactor: cleaned up old jQuery junk

--- app/scripts/ftss-jquery-bindings.js
+++ app/scripts/ftss-jquery-bindings.js
@@ -1,70 +1,25 @@
-/*global FTSS, _ */
+/*global FTSS */
 
-/**
- * jQuery .on() bindings
- */
-(function () {
+'use strict';
 
-	'use strict';
+window.addEventListener('dragover', function (e) { e.preventDefault() }, false);
 
-	/**
-	 * prevent dragover & drop from exiting the application if a user misses the drop target
-	 */
-	window.addEventListener("dragover", function (e) {
-		e = e || event;
+window.addEventListener('drop', function (e) { e.preventDefault() }, false);
+
+document.addEventListener('past', function (e) {
+	if (FTSS.pasteAction) {
+
+		e.stopImmediatePropagation();
 		e.preventDefault();
-	}, false);
+		FTSS.pasteAction((window.clipboardData || e.originalEvent.clipboardData).getData('Text'));
 
-	window.addEventListener("drop", function (e) {
-		e = e || event;
-		e.preventDefault();
-	}, false);
+	}
+});
 
-	(function ($) {
-		$.event.special.destroyed = {
-			remove: function (el) {
-				el.handler && el.handler(this);
-			}
-		};
-	})(jQuery);
-
-	var pasteAction, popoverService;
-
-	FTSS.ng.run([
-		'$popover',
-
-		function ($popover) {
-
-			popoverService = $popover;
-
+(function ($) {
+	$.event.special.destroyed = {
+		remove: function (el) {
+			el.handler && el.handler(this);
 		}
-	]);
-
-	/**
-	 * Intercepts paste events and handles if we have a paste handler set (FTSS.pasteAction)
-	 *
-	 * @param e jQuery event
-	 */
-	pasteAction = function (e) {
-
-		if (FTSS.pasteAction) {
-
-			e.stopImmediatePropagation();
-			e.preventDefault();
-			FTSS.pasteAction((window.clipboardData || e.originalEvent.clipboardData).getData('Text'));
-
-		}
-
 	};
-
-	// Use jQuery on() to bind to future elements
-	$(document)
-
-		.on('click', '.slideToggleEffect *', function (evt) {
-			    evt.stopImmediatePropagation();
-			    $(this).parents('.slideToggleEffect').toggleClass('slideOut');
-		    })
-
-		.on('paste', '*', pasteAction);
-
-}());
+})(jQuery);
