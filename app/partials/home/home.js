df4f07ce81efec7723616a9037598b47e68f7811
Jeff McCoy <code@jeffandshauna.net>
2015-04-26 20:47:50 -0400

    feature: added google chrome detection/user message

--- app/partials/home/home.js
+++ app/partials/home/home.js
@@ -14,6 +14,8 @@
 				// Add to the async handler in case this returns first (likely)
 				utilities.addAsync(function () {
 
+					$scope.notChrome = !window.chrome;
+
 					$scope.courseUpdates = _.filter(caches.MasterCourseList, 'updated');
 
 					utilities.setLoaded();
