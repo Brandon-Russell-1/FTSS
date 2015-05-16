d819fcc953c264b10e46d199b99305e3639135e8
Jeff McCoy <code@jeffandshauna.net>
2015-05-16 00:01:39 -0400

    feature: still working requirements redesign
    - Removed student lookup/names, just numbers is the right solution,
    we’ll do some other work to ease the process later on, but numbers
    really does make the most sense.
    - Finished stats processing work
    - Added remaining missing fields from 898
    - Reworked layout to maximize screen realestate

--- app/partials/backlog/backlog.js
+++ app/partials/backlog/backlog.js
@@ -14,46 +14,26 @@ FTSS.ng.controller(
 
 		function ($scope, SharePoint, $timeout, controllerHelper, utilities, loading, geodata) {
 
+			var self;
+
 			utilities.addAsync(generateRequirements);
 
 			function generateRequirements() {
 
 				loading(true);
 
-				var self = controllerHelper($scope, {
-
-					'sort': 'Number',
-
-					'group': 'MDS',
-
-					'model': 'requirements_stats',
-
-					'filter': 'HostId eq ' + $scope.host.Id,
-
-					'finalProcess': function (groups) {
-
-
-					}
-
-				});
-
-				$scope.data = {
-					'month': moment().add(3, 'months')
-				};
-
-				$scope.old = {};
-
 				$scope.history = {};
 
-				$scope.data.Course_Requirements = [];
-
+				// Load the host data
 				$scope.myHost = caches.Hosts[$scope.host.Id];
 
 				// Using the host.FTD property (if it exists) add the ftd object
 				$scope.myFTD = $scope.myHost.FTD ? caches.Units[$scope.myHost.FTD] : false;
 
+				// Use our FTD's location if assigned to one
 				$scope.myHost.Location = $scope.myFTD.Location || $scope.myFTD;
 
+				// Create a list of available FTDs sorted by distance
 				$scope.unitList = _.map(angular.copy(caches.Units), function (unit) {
 
 					geodata.distances(unit, $scope.myHost.Location, unit.Location);
@@ -65,32 +45,52 @@ FTSS.ng.controller(
 
 				});
 
-				self.bind().then(function (backlogStats) {
+				$scope.data = {
 
-					// Iterate over our stats data--this will tell us if a user has already been submitted before and track our history
-					_.each(backlogStats, function (stat) {
+					// collection of course requirements
+					'Course_Requirements': [],
 
-						// This will let us have multiple 898's for one month
-						var history = $scope.history[stat.Month] = $scope.history[stat.Month] || {};
+					// Default to 3 months out
+					'month': moment().add(3, 'months'),
 
-						// Iterate over all the courses in an 898
-						_.each(stat.Data_JSON, function (course, id) {
+					// Default to the host's local FTD
+					'targetFTD': $scope.myFTD && $scope.myFTD.Id
+				};
 
-							// Build our list of trainee requests so we don't show prior requests still in the AAA
-							$scope.old[id] = $scope.old[id] ? $scope.old[id].concat(course[1]) : course[1];
+				$scope.$watch('data.targetFTD', function (ftd) {
 
-							// This will let us have duplicate course requests in one month
-							var h = history[id] = history[id] || {'built': 0, 'required': 0};
+					if (ftd) {
 
-							// We have to make -1 a 0 (the default is -1 when FTD hasn't responded)
-							h.built += (course[0] < 1) ? 0 : course[0];
-							h.required += course[1].length;
+						// Bind the unit.courses to coursesDropdown for selectize
+						$scope.coursesDropdown = caches.Units[ftd].Courses;
+
+						self = controllerHelper($scope, {
+
+							'sort': 'Number',
+
+							'group': 'MDS',
+
+							'model': 'requirements_stats',
+
+							'filter': [
+								'HostId eq ' + $scope.host.Id,
+								'FTDId eq ' + ftd
+							].join(' and ')
 
 						});
 
-					});
+						self.bind().then(function (backlogStats) {
+
+							$scope.history = _.groupBy(backlogStats, 'Month');
+
+							processCourseRequirements();
+
+						});
+
+					}
 
 				});
+
 
 				/**
 				 * Used by the requirements wizard to report current progress for the user
@@ -108,7 +108,7 @@ FTSS.ng.controller(
 
 						($scope.ftss.itemCount.value > 0),
 
-						$scope.groups ? _($scope.flatData).pluck('peopleCount').sum() : false,
+						$scope.groups ? _($scope.groups).pluck('required').sum() : false,
 
 						(($scope.requests || {}).count > 0)
 
@@ -141,16 +141,7 @@ FTSS.ng.controller(
 
 				};
 
-				$scope.getOver = function (row) { return row.peopleCount > row.Max };
-
-				$scope.getUnder = function (row) { return row.peopleCount < row.Min };
-
-				$scope.$watch('data.targetFTD', function (ftd) {
-
-					// Bind the unit.courses to coursesDropdown for selectize
-					$scope.coursesDropdown = ftd && caches.Units[ftd].Courses;
-
-				});
+				$scope.getUnder = function (row) { return row.required < row.Min };
 
 				$scope.$watch('data.month', function (month) {
 
@@ -164,33 +155,89 @@ FTSS.ng.controller(
 							labels.push(instance.add(1, 'months').format('MMM YYYY'));
 						});
 
+						processCourseRequirements();
 
 					}
 
 				});
 
-				$scope.$watch('data.Course_Requirements', function (text) {
+				$scope.$watch('data.Course_Requirements', processCourseRequirements);
 
-					if ($scope.loaded && text) {
+				function processCourseRequirements(text) {
 
-						var courses = {};
+					text = text || $scope.courses;
+
+					if ($scope.loaded && self && text) {
+
+						var refMonth = $scope.data.month.clone().add(-5, 'months'),
+
+							months = _.times(3, function () {
+								return refMonth.add(1, 'months').format('YYYY.MM');
+							}),
+
+							courses = {};
+
+						$scope.courses = text;
 
 						// Iterate over all the requirements
 						_.each(text, function (courseId) {
 
 							courses[courseId] = angular.copy(caches.MasterCourseList[courseId]);
 
+							courses[courseId].History = [
+								[0, 0],
+								[0, 0],
+								[0, 0]
+							]
+						});
+
+						// Iterate over our stats data--this will tell us if a user has already been submitted before and track our history
+						_.each($scope.history, function (stats, month) {
+
+							var index = months.indexOf(month);
+
+							// Only look at the last three months history
+							if (index > -1) {
+
+								// Consolidate our history data and handle multiple requests in the same month
+								_(stats).pluck('Data_JSON').each(function (statGroup) {
+
+									// Iterate over each course in the history for this month
+									_.each(statGroup, function (stat, courseId) {
+
+										// Iterate over each course in the list
+										_.each(courses, function (course) {
+
+											// If a course matches the history for this month, use loose typing to match Number == String
+											if (course.Id == courseId) {
+
+												// Update our stats for the match
+												course.History[index][0] += stat[0];
+												course.History[index][1] += stat[1];
+
+											}
+
+										});
+
+									});
+
+								}).value();
+
+							}
+
 						});
 
 						self.initialize(courses).then(function (d) {
 
-							d.peopleCount = 0;
+							d.backlog = 0;
+							d.backlogPriority = 0;
+							d.required = 0;
 
 						});
 
 					}
 
-				});
+				}
 
 			}
 
