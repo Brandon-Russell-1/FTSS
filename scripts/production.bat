7cd6b584f6d27c4997dd24a77b9e8ac604d18793
Jeff McCoy <code@jeffm.us>
2015-07-14 06:31:53 +0200

    Chore: still working the production build

--- scripts/production.bat
+++ scripts/production.bat
@@ -2,8 +2,6 @@ rm -fR
 
 node_modules\.bin\brunch build -P
 
-pause
+node_modules\.bin\jade app\_app.jade -o _public
 
-node_modules\.bin\jade app\_app.jade
-
-mv app\_app.html _public\app.html
+mv _public\_app.html _public\app.html
