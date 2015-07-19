53d5e26f8631e4761bcd7f9bac605693d811f579
Jeff McCoy <code@jeffm.us>
2015-07-19 13:18:20 +0200

    FIx: greatly simplified traverse directive/fixed issues navigating in various views
    Dropped all the jQuery reliance and now use only JS arrays/objects to do
    the traverasl action.  Beyond greatly reducing the needed code, this
    also should make the operations more reliable + more flexible as it is
    view-independent.

--- app/partials/components/traverse/traverse.js
+++ app/partials/components/traverse/traverse.js
@@ -17,64 +17,45 @@
 				return {
 					'restrict'   : 'E',
 					'templateUrl': '/partials/traverse.html',
-					'link'       : function (scope, $el) {
+					'link'       : function (scope, $el, $attr) {
 
-						var view = $('#mainView'),
-
-						    content = $('#content'),
-
-							data = {};
+						var data = {};
 
 						if (!scope.createData) {
 
-							// Let the view CSS know about this modal so we can do some fun stuff
-							view.addClass('hasModal');
+							if ($attr.bind) {
 
-							// Make a flat copy of our data
-							_.each(scope.groups, function (group) {
-								_.each(group, function (row) {
-									data[row.Id] = row;
+								data = scope[$attr.bind];
+
+							} else {
+
+								data = [];
+
+								// Make a flat copy of our data
+								_.each(scope.groups, function (group) {
+									_.each(group, function (row) {
+										data.push(row);
+									});
 								});
-							});
+
+							}
 
 							// Bind the traverse action which allows navigating between records without closing/opening the modal
-							scope.traverse = scope.update(scope, function (forward) {
+							scope.traverse = scope.update(scope, function () {
 
-								// Wrap in $timeout due to the async callback required--this was simpler than a promise
 								$timeout(function () {
 
-									var rows, row, pointer;
+									// Find our current record
+									var index = _.findIndex(data, {'Id': scope.data.Id});
 
-									// Array of all tr's on the page (with a scope)
-									rows = $('tr.traverse');
-
-									// The currently selected row
-									row = $('[traverse="' + scope.data.Id + '"]');
-
-									// Our current row index
-									pointer = rows.index(row);
-
-									// Map to the new row dependent on the forward variable
-									if (forward) {
-										row = rows.eq(++pointer).length && rows.eq(pointer) || rows.first();
-									} else {
-										row = rows.eq(--pointer).length && rows.eq(pointer) || rows.last();
-									}
-
-									// Copy data back into new scope.data variable
-									scope.data = data[parseInt(row.attr('traverse'), 10)];
+									// Go foward if able, otherwise, start over
+									scope.data = data[index + 1] || data[0];
 
 									// Reset the form state
 									scope.modal.$setPristine();
 
 								});
 
-							});
-
-							// Watch for the destruction of this element and then remove .modal-selected class
-							$el.on("$destroy", function () {
-								view.removeClass('hasModal');
-								$('tr.traverse').removeClass('modal-selected');
 							});
 
 						} else {
