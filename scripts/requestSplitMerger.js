83a8873960ee13b9beaafc66928a208615226471
Jeff McCoy <code@jeffm.us>
2015-07-24 00:32:02 +0200

    chore: draft request migration tool
    This is going to split the requests list items.  Pending/denied stay the
    same, but approved will be moved to the actual scheduled list item.
    This will give us new flexibility (could be the last thing needed to
    enable PII) while maintaining a least-privilege model for SP
    permissions.

---
+++ scripts/requestSplitMerger.js
@@ -0,0 +1,38 @@
+/**
+ * This is a helper to migrate approvals back to the scheduled list, part of the reservation system
+ *
+ * The requests lists will still hold pending/denied requests, but approvals will be moved to the scheduled list to allow privilege seperation
+ */
+
+
+var send = {};
+
+FTSS.SharePoint.read({
+	'source': 'Requests',
+	'params': {
+		'$filter': "Class/Timestamp ne null and Status eq 'Approved'",
+		'$expand': [
+			'Class'
+		],
+		'$select': [
+			'HostId',
+			'Notes',
+			'Response',
+			'Students_JSON',
+			'Class/Timestamp'
+		]
+	}
+
+}).then(function (data) {
+	_.each(data, function (row) {
+		send[row.Class.__metadata.uri] = send[row.Class.__metadata.uri] || {cache: true, __metadata: row.Class.__metadata, Students_JSON: []};
+
+		send[row.Class.__metadata.uri].Students_JSON.push({HostId: row.HostId, Host: row.Notes, FTD: row.Response, Students: row.Students_JSON});
+	});
+
+});
+
+var batchSend = _.toArray(send)
+
+
+FTSS.SharePoint.batch(batchSend.slice(0, 99));
